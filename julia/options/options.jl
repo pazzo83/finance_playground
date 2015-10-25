30412e4491fc37b354f6af31fb60c3688fe05b53
pazzo83 <uvapazzo@gmail.com>
2015-10-25 17:06:33 -0400

    adding full binomial pricing model

--- julia/options/options.jl
+++ julia/options/options.jl
@@ -25,41 +25,82 @@ type StockOption
 end
 
 # build binomial tree
-function build_tree(s_opt::StockOption)
-  u = 1 + s_opt.pu
-  d = 1 - s_opt.pd
-  qu = (exp((s_opt.r - s_opt.div) * s_opt.dt) - d) / (u - d)
-  qd = 1 - qu
+# function build_tree(s_opt::StockOption)
+#   u = 1 + s_opt.pu
+#   d = 1 - s_opt.pd
+#   qu = (exp((s_opt.r - s_opt.div) * s_opt.dt) - d) / (u - d)
+#   qd = 1 - qu
+#
+#   # init array
+#   stockTree = Vector{Float64}[ zeros(m) for m = 1:s_opt.N + 1]
+#
+#   for i = 1:s_opt.N + 1
+#     for j = 1:i
+#       stockTree[i][j] = s_opt.S0 * u ^ (j-1) * d ^ (i - j)
+#     end
+#   end
+#
+#   return stockTree
+# end
 
-  # init array
-  stockTree = Vector{Float64}[ zeros(m) for m = 1:s_opt.N + 1]
+# function build_tree_matrix(s_opt::StockOption)
+#   u = 1 + s_opt.pu
+#   d = 1 - s_opt.pd
+#   qu = (exp((s_opt.r - s_opt.div) * s_opt.dt) - d) / (u - d)
+#   qd = 1 - qu
+#
+#   # init Matrix
+#   stockMatrix = zeros(s_opt.N + 1, s_opt.N + 1)
+#
+#   for i =1:size(stockMatrix)[1]
+#     for j = 1:i
+#       stockMatrix[j, i] = s_opt.S0 * u ^ (j-1) * d ^ (i - j)
+#     end
+#   end
+#
+#   return stockMatrix
+# end
 
-  for i = 1:s_opt.N + 1
-    for j = 1:i
-      stockTree[i][j] = s_opt.S0 * u ^ (j-1) * d ^ (i - j)
-    end
+function check_early_exercise(payoff::Float64, ST::Float64, K::Float64, is_call::Bool)
+  return max(payoff, is_call ? ST - K : K - ST)
+end
+
+function build_tree_matrix(S0::Float64, n::Int64, u::Float64, d::Float64)
+  stockTree = zeros(n + 1, n + 1)
+
+  for i = 1:n + 1, j = 1:n + 1
+    stockTree[i, j] = S0 * u ^ (j-1) * d ^ (i - 1)
   end
 
   return stockTree
 end
 
-function build_tree_matrix(s_opt::StockOption)
+function option_value_tree_matrix(K::Float64, n::Int64, binom::Array, qu::Float64, qd::Float64, df::Float64, is_call::Bool, is_euro::Bool)
+  payoffs = zeros(n + 1, n + 1)
+
+  for i = 1:n + 1
+    payoffs[n+1 - (i - 1), i] = max(0, is_call ? binom[n+1 - (i - 1), i] - K : K - binom[n+1 - (i - 1), i])
+  end
+
+  for i = n:-1:1, j = n - (i - 1):-1:1
+    payoffs[i,j] = (qu * payoffs[i, j+1] + qd * payoffs[i+1, j]) * df
+
+    if ! is_euro
+      payoffs[i,j] = check_early_exercise(payoffs[i,j], binom[i,j], K, is_call)
+    end
+  end
+
+  return payoffs
+end
+
+function price(s_opt::StockOption)
   u = 1 + s_opt.pu
   d = 1 - s_opt.pd
   qu = (exp((s_opt.r - s_opt.div) * s_opt.dt) - d) / (u - d)
   qd = 1 - qu
 
-  # init Matrix
-  stockMatrix = zeros(s_opt.N + 1, s_opt.N + 1)
+  binom_tree = build_tree_matrix(s_opt.S0, s_opt.N, u, d)
+  payoffs = option_value_tree_matrix(s_opt.K, s_opt.N, binom_tree, qu, qd, s_opt.df, s_opt.is_call, s_opt.is_euro)
 
-  for i =1:size(stockMatrix)[1]
-    for j = 1:i
-      stockMatrix[j, i] = s_opt.S0 * u ^ (j-1) * d ^ (i - j)
-    end
-  end
-
-  return stockMatrix
-end
-
-function option_value_tree(s_opt::StockOption, binom::Array{Array{Float64,1},1})
+  return payoffs[1,1]
 end
