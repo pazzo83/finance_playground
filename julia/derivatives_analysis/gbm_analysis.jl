da6bcbad5fa611d6a0e114d1d558d5ec3260d54b
Christopher Alexander <uvapazzo@gmail.com>
2016-04-27 22:23:18 -0400

    adding more derivs analysis

---
+++ julia/derivatives_analysis/gbm_analysis.jl
@@ -0,0 +1,53 @@
+using DataFrames
+using TimeSeries
+
+function dN(x::Float64, mu::Float64, sigma::Float64)
+  z = (x - mu) / sigma
+  pdf = exp(-0.5 * x ^ 2) / sqrt(2 * pi * sigma^2)
+
+  return pdf
+end
+
+# Simulate a Number of Years of Daily Stock Quotes
+
+function simulate_gbm()
+  # Model params
+  S0 = 100.0 # Initial index level
+  T = 10.0 # time horizon
+  r = 0.05 # risk-less short rate
+  vol = 0.2 # instantaneous volatility
+
+  # simulation params
+  rng = MersenneTwister(250000)
+
+  all_dates = Date(2004, 9, 30):Date(2014, 9, 30)
+
+  gbm_dates = all_dates[Dates.dayofweek(all_dates) .< 6]
+
+  M = length(gbm_dates) # Time steps
+  I = 1 # index level paths
+  dt = 1 / 252 # fixed for simplicity
+  df = exp(-r * dt) # discount factor
+
+  # stock price paths
+  rands = randn(M, I)
+  S = zeros(rands)
+  S[1] = S0
+  for t = 2:M
+    S[t] = S[t-1] * exp((r - vol ^ 2 / 2) * dt + vol * rands[t] * sqrt(dt))
+  end
+
+  # gbm = DataFrame(Date=gbm_dates, index=S[:, 1])
+  gbmTimeSeries = TimeArray(gbm_dates, S[:, 1], ["index"])
+
+  rets = log(gbmTimeSeries["index"] ./ lag(gbmTimeSeries["index"], padding=true))
+
+  gbm = DataFrame(Date=gbm_dates)
+  gbm[:returns] = rets.values
+  gbm[isnan(gbm[:returns]), :returns] = 0.0
+  # Realized volatility (e.g. as defined for variance swaps)
+  gbm[:rea_var] = 252 * cumsum(gbm[:returns] .^ 2) ./ range(1, M)
+  gbm[:rea_vol] = sqrt(gbm[:rea_var])
+
+  return gbm
+end
---
+++ julia/derivatives_analysis/historical_data.jl
@@ -0,0 +1,91 @@
+using Base.Dates
+using Requests
+using DataFrames
+
+abstract DataSource
+immutable YahooDataSource <: DataSource end
+
+# type YahooFinanceQuery{S <: AbstractString}
+#   query::S
+#   responseJSON::S
+# end
+#
+# type YahooDataSet{S <: AbstractString}
+#   rawJson::S
+#   dates::Vector{Date}
+#   lows::Vector{Float64}
+#   highs::Vector{Float64}
+#   opens::Vector{Float64}
+#   closes::Vector{Float64}
+#   volumes::Vector{Float64}
+#   adjCloses::Vector{Float64}
+#   symbs::Vector{S}
+# end
+
+# function get_historical_data(::YahooDataSource, startDate::Date, endDate::Date, symbol::AbstractString)
+#   # static dates right now
+#   # date1 = Date(2009, 9, 11)
+#   # date2 = Date(2010, 3, 10)
+#   # symb = "YHOO"
+#
+#   # construct query
+#   url = "https://query.yahooapis.com/v1/public/yql?q="
+#
+#   query = "select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22$symbol%22%20and%20startDate%20%3D%20%22$(string(startDate))%22%20and%20endDate%20%3D%20%22$(string(endDate))%22"
+#
+#   resp = get(string(url, query, "&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="))
+#
+#   respJson = Requests.json(resp)["query"]["results"]["quote"]
+#
+#   n = length(respJson)
+#   dates = Vector{Date}(n)
+#   lows = Vector{Float64}(n)
+#   highs = Vector{Float64}(n)
+#   opens = Vector{Float64}(n)
+#   closes = Vector{Float64}(n)
+#   volumes = Vector{Float64}(n)
+#   adjCloses = Vector{Float64}(n)
+#   symbs = Vector{ASCIIString}(n)
+#
+#   for i in eachindex(respJson)
+#     dates[i] = Date(respJson[i]["Date"])
+#     lows[i] = float(respJson[i]["Low"])
+#     highs[i] = float(respJson[i]["High"])
+#     opens[i] = float(respJson[i]["Open"])
+#     closes[i] = float(respJson[i]["Close"])
+#     volumes[i] = float(respJson[i]["Volume"])
+#     adjCloses[i] = float(respJson[i]["Adj_Close"])
+#     symbs[i] = respJson[i]["Symbol"]
+#   end
+#
+#   df = DataFrame(Date = dates, Low = lows, High = highs, Close = closes, Volume = volumes, AdjClose = adjCloses, Symbol = symbs)
+#
+#   return df
+# end
+
+function get_historical_data(::YahooDataSource, startDate::Date, endDate::Date, symbol::AbstractString)
+  startDate < endDate || error("dates are wrong")
+  
+  url = "http://ichart.finance.yahoo.com/table.csv?s="
+
+  query = "$symbol&d=$(Int(Month(endDate)) - 1)&e=$(Int(Day(endDate)))&f=$(Int(Year(endDate)))&g=d&a=$(Int(Month(startDate)) - 1)&b=$(Int(Day(startDate)))&c=$(Int(Year(startDate)))"
+
+  # resp = get(url, query, "&ignore.csv")
+  #
+  # respCSV = readall(resp)
+
+  urlFull = string(url, query, "&ignore.csv")
+
+  # println(urlFull)
+
+  r = get(urlFull)
+
+  # println(r.response)
+
+  # df = readtable(get_streaming(string(url, query, "&ignore.csv")))
+
+  df = readtable(IOBuffer(readall(r)))
+  df[:Date] = Date(df[:Date])
+
+  return df
+end
